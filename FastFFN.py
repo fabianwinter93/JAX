104fe861665a6098550e09a0f4457371244f9851
root <fabianwinter93@gmail.com>
2023-11-23 12:17:44 +0000

    fastFFN cleaner version that works

--- FastFFN.py
+++ FastFFN.py
@@ -37,9 +37,13 @@ class FastFFN(nn.Module):
 
     mixture = jnp.ones((B, T, self.num_leaves))
 
-    node_dense = nn.DenseGeneral((self.num_nodes,1), use_bias=self.use_bias, kernel_init=self.node_init(1/jnp.sqrt(D)), param_dtype=self.dtype)
-
-    c = vmap(vmap(lambda u : nn.sigmoid(node_dense(u))))(inputs)
+    node_kernel = self.param("node_kernel", self.node_init(1/jnp.sqrt(D)), (D, self.num_nodes,), self.dtype)
+    
+    if self.use_bias:
+      node_bias = self.param("node_bias", init.zeros_init(), (self.num_nodes,), self.dtype)
+      c = vmap(vmap(lambda u : nn.sigmoid(u @ node_kernel + node_bias)))(inputs)
+    else:
+      c = vmap(vmap(lambda u : nn.sigmoid(u @ node_kernel)))(inputs)
 
     """
     if training:
@@ -62,11 +66,37 @@ class FastFFN(nn.Module):
 
     mixture = mixture.reshape((B, T, self.num_leaves))
 
-    l1a = nn.DenseGeneral((self.num_leaves, self.leaf_dim), kernel_init=self.leaf_init(1/jnp.sqrt(D)), use_bias=self.use_bias, param_dtype=self.dtype) 
-    l1b = nn.DenseGeneral((self.num_leaves, self.leaf_dim), kernel_init=self.leaf_init(1/jnp.sqrt(D)), use_bias=self.use_bias, param_dtype=self.dtype) 
-    l2 = nn.DenseGeneral((self.num_leaves, self.dim), axis=(-1,-2), kernel_init=self.leaf_init(1/jnp.sqrt(self.leaf_dim)), use_bias=self.use_bias, param_dtype=self.dtype) 
+    w_leaves_1_a = [self.param(f"weight_leaf_1a_{i}", self.leaf_init(1/jnp.sqrt(D)), (D, self.leaf_dim), self.dtype) for i in range(self.num_leaves)]
+    w_leaves_1_b = [self.param(f"weight_leaf_1b_{i}", self.leaf_init(1/jnp.sqrt(D)), (D, self.leaf_dim), self.dtype) for i in range(self.num_leaves)]
+    w_leaves_2 = [self.param(f"weight_leaf_2_{i}", self.leaf_init(1/jnp.sqrt(self.leaf_dim)), (self.leaf_dim, D), self.dtype) for i in range(self.num_leaves)]
+    
+    w1a = jnp.stack(w_leaves_1_a, 0)
+    w1b = jnp.stack(w_leaves_1_b, 0)
+    w2 = jnp.stack(w_leaves_2, 0)
 
-    y = vmap(vmap(lambda u : l2(l1a(u)*l1b(u))))(inputs)
+    if self.use_bias:
+      b_leaves_1_a = [self.param(f"bias_leaf_1a_{i}", init.zeros_init(), (self.leaf_dim,), self.dtype) for i in range(self.num_leaves)]
+      b_leaves_1_b = [self.param(f"bias_leaf_1b_{i}", init.zeros_init(), (self.leaf_dim,), self.dtype) for i in range(self.num_leaves)]
+      b_leaves_2 = [self.param(f"bias_leaf_2_{i}", init.zeros_init(), (D,), self.dtype) for i in range(self.num_leaves)]
+      
+      b1a = jnp.stack(b_leaves_1_a, 0)
+      b1b = jnp.stack(b_leaves_1_b, 0)
+      b2 = jnp.stack(b_leaves_2, 0)
+
+    
+    
+    if self.use_bias:
+      leaf_f_1 = vmap(lambda u,wa,wb,ba,bb : (u @ wa + ba) * (u @ wb + bb), (None, 0, 0, 0, 0))
+      leaf_f_2 = vmap(lambda u,w,b : u @ w + b, (-2, 0, 0))
+      y = vmap(vmap(lambda u : leaf_f_1(u, w1a, w1b, b1a, b1b)))(inputs)
+      y = vmap(vmap(lambda u : leaf_f_2(u, w2, b2)))(y)
+    else:
+      leaf_f_1 = vmap(lambda u,wa,wb : (u @ wa) * (u @ wb), (None, 0, 0))
+      leaf_f_2 = vmap(lambda u,w: u @ w, (-2, 0))
+
+      y = vmap(vmap(lambda u : leaf_f_1(u, w1a, w1b)))(inputs)
+      y = vmap(vmap(lambda u : leaf_f_2(u, w2)))(y)
+
     
     y = y * jnp.expand_dims(mixture, -1)
     y = y.sum(-2)
