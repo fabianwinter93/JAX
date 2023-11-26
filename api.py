6066ce3559337c11ffe7b958caf38ab171eb5928
root <fabianwinter93@gmail.com>
2023-11-26 17:01:22 +0000

    bug fix

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
-from .code.ChannelMixers.FastFeedForwardNetwork import FastFeedForwardNetwork, tests as tests_fffn
-from .code.TimeMixers.GateLoop import GateLoop, tests as tests_GL
-from .code.TimeMixers.LinearRecurrentUnit import LinearRecurrentUnit, tests as tests_LRU 
-from .code.TimeMixers.QLSTM import QLSTM, tests as tests_QLSTM
-from .code.TimeMixers.SuperHighPerformanceExtractor import SuperHighPerformanceExtractor, tests as tests_SHE
+from ...code.ChannelMixers.FastFeedForwardNetwork import FastFeedForwardNetwork, tests as tests_fffn
+from ...code.TimeMixers.GateLoop import GateLoop, tests as tests_GL
+from ...code.TimeMixers.LinearRecurrentUnit import LinearRecurrentUnit, tests as tests_LRU 
+from ...code.TimeMixers.QLSTM import QLSTM, tests as tests_QLSTM
+from ...code.TimeMixers.SuperHighPerformanceExtractor import SuperHighPerformanceExtractor, tests as tests_SHE
 
 
 tests = [tests_fffn, tests_GL, tests_LRU, tests_QLSTM, tests_SHE]
