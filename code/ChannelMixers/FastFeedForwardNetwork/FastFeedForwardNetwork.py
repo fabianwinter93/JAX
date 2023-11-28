a7a093e200746157b25f51181374cbee2a7861f4
root <fabianwinter93@gmail.com>
2023-11-28 17:50:05 +0000

    update fffn, add inference mode

--- code/ChannelMixers/FastFeedForwardNetwork/FastFeedForwardNetwork.py
+++ code/ChannelMixers/FastFeedForwardNetwork/FastFeedForwardNetwork.py
@@ -12,53 +12,14 @@ from typing import (
 )
 vmap = jax.vmap
 
-class FFFNMLP(nn.Module):
-  dim : int
-  use_bias : bool
-  activation : Callable
-  dtype : jax.typing.DTypeLike
-
-
-  def param_init(self, scale):
-    def f(key, shape, dtype):
-      _scale = 1 / jnp.sqrt(scale)
-      val = random.uniform(key, shape=shape, minval=-_scale, maxval=_scale)
-      return val.astype(dtype)
-    return f
-
-  @nn.compact
-  def __call__(self, inputs, training, *args, **kwargs):
-    D = inputs.shape[0]
-
-    w1 = self.param("kernel_1", self.param_init(D), (D, self.dim*2), self.dtype)
-    w2 = self.param("kernel_2", self.param_init(self.dim), (self.dim, D), self.dtype)
-
-    if self.use_bias:
-      b1 = self.param("bias_1", self.param_init(D), (self.dim*2,), self.dtype)
-      b2 = self.param("bias_2", self.param_init(self.dim), (D,), self.dtype)
-      
-
-    if self.use_bias:
-      leaf_f_1 = lambda u : self.activation(u @ w1 + b1)
-      leaf_f_2 = lambda u : u @ w2 + b2
-
-    else:
-      leaf_f_1 = lambda u : self.activation(u @ w1)
-      leaf_f_2 = lambda u : u @ w2
-      
-    y = leaf_f_1(inputs)
-    y = leaf_f_2(y)
-
-    return y
-
-
 class FastFeedForwardNetwork(nn.Module):
   output_dim : int
   leaf_dim : int
   depth : int
-  
+
   activation : Callable = lambda u : u[u.shape[0]//2:] * u[:u.shape[0]//2]
   use_bias : bool = True
+  use_ste : bool = True
 
   dtype : jax.typing.DTypeLike = jnp.float32
 
@@ -73,29 +34,92 @@ class FastFeedForwardNetwork(nn.Module):
     self.num_leaves = 2**self.depth
     self.num_nodes = self.num_leaves - 1
 
+    #self.leaves = [FFFNMLP(self.leaf_dim, self.use_bias, activation=self.activation, dtype=self.dtype) for _ in range(self.num_leaves)]
+
+
+
   @nn.compact
   def __call__(self, inputs, training, *args, **kwargs):
     B, T, D = inputs.shape
 
+
+    w1 = self.param("kernel_1", self.param_init(D), (self.num_leaves, D*self.leaf_dim*2), self.dtype)
+    w2 = self.param("kernel_2", self.param_init(self.leaf_dim), (self.num_leaves, self.leaf_dim*self.output_dim), self.dtype)
+
+    if self.use_bias:
+      b1 = self.param("bias_1", self.param_init(D), (self.num_leaves,self.leaf_dim*2), self.dtype)
+      b2 = self.param("bias_2", self.param_init(self.leaf_dim), (self.num_leaves,self.output_dim), self.dtype)
+
+    wnode = self.param("kernel_node", self.param_init(D), (self.num_nodes, D), self.dtype)
+    bnode = self.param("bias_node", self.param_init(D), (self.num_nodes,1), self.dtype)
+
     ## layer logic ##
-    
-    def fwd(l, m, n):
+
+    def fwd_train(l, m, n):
       if m == self.depth:
-        mlp = FFFNMLP(self.leaf_dim, self.use_bias, activation=self.activation, dtype=self.dtype)
-        y = mlp(l, training=training)
+        _w1 = w1[n,:].reshape((D, self.leaf_dim*2))
+        _w2 = w2[n,:].reshape((self.leaf_dim, self.output_dim))
+        
+        if self.use_bias:
+          _b1 = b1[n,:].reshape((self.leaf_dim*2,))
+          _b2 = b2[n,:].reshape((self.output_dim,))
+
+        y = l @ _w1
+        if self.use_bias:
+          y = y + _b1
+        y = self.activation(y)
+        y = y @ _w2
+        if self.use_bias:
+          y = y + _b2
         return y
       else:
-        node = nn.Dense(1, kernel_init=self.param_init(D), bias_init=self.param_init(D), use_bias=self.use_bias, param_dtype=self.dtype)
-        c = nn.sigmoid(node(l))
+        
+        w = wnode[m,:]          
+        b = bnode[m,:]
+        c = l @ w + b
+
+        c = nn.sigmoid(c)
+
+        # STE
+        if self.use_ste:
+          c = c - jax.lax.stop_gradient(c) + jax.lax.stop_gradient(jnp.rint(c).astype(int))
+
         return c * fwd(l, m+1, 2*n) + (1-c) * fwd(l, m+1, 2*n + 1)
+
+    def fwd_inference(l, m, n):
+      if m == self.depth:
+        _w1 = w1[n,:].reshape((D, self.leaf_dim*2))
+        _w2 = w2[n,:].reshape((self.leaf_dim, self.output_dim))
+        
+        if self.use_bias:
+          _b1 = b1[n,:].reshape((self.leaf_dim*2,))
+          _b2 = b2[n,:].reshape((self.output_dim,))
+
+        y = l @ _w1
+        if self.use_bias:
+          y = y + _b1
+        y = self.activation(y)
+        y = y @ _w2
+        if self.use_bias:
+          y = y + _b2
+        return y
+      else:
+
+        w = wnode[m,:]
+        b = bnode[m,:]
+        c = l @ w + b
+
+        c = nn.sigmoid(c)
+        c = jnp.rint(c).astype(int)
+
+        return fwd(l, m+1, 2*n + c)# + (1-c) * fwd(l, m+1, 2*n + 1)
+
+    if training:
+      fwd = fwd_train
+    else:
+      fwd = fwd_inference
 
     y = vmap(vmap(lambda u : fwd(u, 0, 0)))(inputs)
 
+
     return y
-
-
-
-
-
-
-
--- code/ChannelMixers/FastFeedForwardNetwork/tests.py
+++ code/ChannelMixers/FastFeedForwardNetwork/tests.py
@@ -3,7 +3,7 @@ from jax import numpy as jnp
 import jax
 
 if __name__ == "__main__":
-    
+
   B = 32
   T = 128
   Din = 256
@@ -15,6 +15,7 @@ if __name__ == "__main__":
 
   jit_options = [False, True]
   bias_options = [False, True]
+  ste_options = [False, True]
 
   key = jax.random.PRNGKey(123)
   inp_key, param_key = jax.random.split(key)
@@ -24,16 +25,19 @@ if __name__ == "__main__":
   for dt in dtypes:
     for jit_op in jit_options:
       for bias_op in bias_options:
-        fffn = FastFeedForwardNetwork(output_dim=Dout, leaf_dim=Dleaf, depth=depth, dtype=dt, use_bias=bias_op)
+        for ste_op in ste_options:
+          fffn = FastFeedForwardNetwork(output_dim=Dout, leaf_dim=Dleaf, depth=depth, dtype=dt, use_bias=bias_op, use_ste=ste_op)
 
-        params = fffn.init(param_key, x, False)
+          params = fffn.init(param_key, x, False)
+          f = fffn.apply
 
-        f = fffn.apply
-        if jit_op:
-          f = jax.jit(f)
-        
-        y = f(params, x, True)
+          if jit_op:
+              f = jax.jit(f, static_argnums=(2))
+          
+          for train_op_a in [False, True]:
+            y = f(params, x, train_op_a)
 
-        print(f"finished dtype={str(dt.dtype)}, jit_op={str(jit_op)}, bias_op={str(bias_op)}")
+            for train_op_b in [False, True]:
+              y = f(params, x, train_op_b)
 
-    
+          print(f"finished dtype={str(dt.dtype)}, jit_op={str(jit_op)}, bias_op={str(bias_op)}, ste_op={str(ste_op)}")
